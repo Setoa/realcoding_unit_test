c253d00df1b623334a69426ba6614c0538e03ee4
Setoa <diqzk528@naver.com>
2020-07-20 16:12:40 +0900

    Unit

--- src/test/java/ChampionTest.java
+++ src/test/java/ChampionTest.java
@@ -17,14 +17,12 @@ public class ChampionTest {
     @Before
     public void setUp() {
 
-        //5개의 챔피언 객체를 만듭니다.
         Champion topChamp = new Champion("다리우스", "탑");
         Champion jungleChamp = new Champion("리신", "정글");
         Champion midChamp = new Champion("르블랑", "미드");
         Champion adcChamp = new Champion("베인", "바텀");
         Champion supportChamp = new Champion("레오나", "바텀");
 
-        //앞서 만든 List 에 각 챔피언을 추가합니다.
         championList.add(topChamp);
         championList.add(jungleChamp);
         championList.add(midChamp);
@@ -32,7 +30,7 @@ public class ChampionTest {
         championList.add(supportChamp);
     }
 
-    //List<String>을 생성하고 값이 비어 있는지를 테스트 empty()
+
     @Test
     public void givenCollectionWhenEmptyCorrect() {
         List<String> emptyList = new ArrayList<>();
@@ -40,7 +38,7 @@ public class ChampionTest {
         assertTrue(emptyList.isEmpty());
     }
 
-    //notNullValue 활용한 테스트
+
     @Test
     public void notNullCheck() {
         String lck = "LCK";
@@ -48,7 +46,7 @@ public class ChampionTest {
         assertFalse(lck==null);
     }
 
-    //nullValue 활용한 테스트
+
     @Test
     public void givenStringWhenNullIsCorrect() {
         String lck = null;
@@ -57,7 +55,7 @@ public class ChampionTest {
     }
 
 
-    //문자열 관련 테스트 anyOf, containsString, endWith
+
     @Test
     public void testForRelatedString() {
         String sampleString1 = "Player Focus";
@@ -69,21 +67,21 @@ public class ChampionTest {
         assertTrue(sampleString2.contains(startString) && sampleString2.contains(endString));
     }
 
-    //부동소수점 범위 closeTo 테스트
+
     @Test
     public void testForFloatingPoint() {
         assertThat(3.14, closeTo(3, 0.2));
         assertThat(4.45, closeTo(4, 0.5));
     }
 
-    //anything 테스트
+
     @Test
     public void shouldNotErrorGetReference() {
         assertThat(championList.get(2), anything());
         assertThat(championList.get(1), anything());
     }
 
-    //객체 크기 검증 테스트 hasSize
+
     @Test
     public void shouldChampionCountFive() {
         assertTrue(championList.size() == 5);
@@ -92,7 +90,7 @@ public class ChampionTest {
         assertFalse(championList.size() != 5);
     }
 
-    //서폿 챔피언은 타릭이어야 한다라는 조건으로 테스트 코드 작성
+
     @Test
     public void shouldSupportChampionIsTaric() {
         Champion supportChamp = new Champion("타릭", "바텀");
@@ -102,7 +100,7 @@ public class ChampionTest {
         assertTrue(supportChamp.getPosition().equals("바텀") && supportChamp.getName().equals("타릭"));
     }
 
-    //hasProperty 활용하여 속성이 포함되어 있는지 테스트
+
     @Test
     public void shouldHasPropertyPosition() {
         assertThat(championList.get(0), hasProperty("position"));
@@ -110,7 +108,7 @@ public class ChampionTest {
         assertThat(championList.get(1), hasProperty("position", equalTo("정글")));
     }
 
-    //hasToString 활용 테스트
+
     @Test
     public void shouldHaveSomeChampName() {
         List<String> champListNames = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가느", "블라디미르");
@@ -118,7 +116,7 @@ public class ChampionTest {
         assertThat(champListNames.get(1), hasToString("애쉬"));
     }
 
-    //property와 value가 같은지 테스트
+
     @Test
     public void shouldHaveSamePropertyAndValue() {
         List<String> championNames1 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
@@ -127,7 +125,7 @@ public class ChampionTest {
         assertTrue(championNames1.containsAll(championNames2));
     }
 
-    //탑 챔피언은 다리우스여야 한다라는 조건으로 테스트 코드 작성, stream 활용예
+
     @Test
     public void shouldTopChampionIsDarius() {
         Optional<Champion> filterdChampion = championList.stream()
