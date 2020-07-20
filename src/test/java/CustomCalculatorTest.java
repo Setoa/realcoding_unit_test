ef2fd7632652d027a899e6034a0e74b880cc4de4
Setoa <diqzk528@naver.com>
2020-07-20 15:47:52 +0900

    Unit Test

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -2,8 +2,11 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 public class CustomCalculatorTest {
 
@@ -15,6 +18,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(25));
+        assertTrue(result==25);
         System.out.println("result :: " + result);
     }
 
@@ -24,6 +29,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(13));
+        assertFalse(result==12);
         System.out.println("result :: " + result);
     }
 
@@ -33,6 +40,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(45));
+        assertTrue(result==45);
         System.out.println("result :: " + result);
     }
 
@@ -42,6 +51,27 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(5));
+        assertTrue(result==5);
         System.out.println("result :: " + result);
     }
+
+    @Test(timeout = 4000)
+    public void timeInMethodTest() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIsEmptyIndexOutOfBoundException(){
+        new ArrayList<Object>().get(0);
+        new ArrayList<Object>().get(1);
+    }
+
+    @Test
+    public void testMethod(){
+        ArrayList<Object> myList=new ArrayList<Object>();
+        assertThat(myList,is(empty()));
+        assertTrue(myList.isEmpty());
+    }
+
 }
